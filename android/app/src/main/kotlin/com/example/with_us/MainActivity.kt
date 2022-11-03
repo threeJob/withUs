7af3c733c282450ab6567220b5321db5612a88bc
wlwl1011 <minji001011@naver.com>
2022-11-03 12:28:26 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/with_us/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.with_us
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xFF222C4F);
+const KGreyColor = Color(0xFF484848);
+const KGreenColor = Color(0xFF14AE5C);
